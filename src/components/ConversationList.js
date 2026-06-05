23f5e341bf12026733c9ba3b32a7600f32ae1f93
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-06-05 13:46:49 -0500

    added ConversationList component

--- src/components/ConversationList.js
+++ src/components/ConversationList.js
@@ -1,3 +1,19 @@
-export default function ConversationList() {
-    
+export default function ConversationList({conversations, selected, setSelected, NewConversation}) {
+    return (
+        <div className="conversation-list-panel">
+            <button 
+                className="new-conversation-button" 
+                onClick={() => {NewConversation}}
+            ><img src="new-chat-icon"/></button>
+            <ul className="conversation-list">
+                {conversations.map(conv => (
+                    <li 
+                        className={selected?.name === conv.name ? "selected-list-item" : "list-item"}
+                        key={conv.name} 
+                        onClick={() => setSelected(conv)}
+                    >{conv.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
