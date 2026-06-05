240548df12e2b4d53f966d7f31073f2fb967a775
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-06-05 01:33:18 -0500

    added ChatInput component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ChatInput from './components/ChatInput';
+import ChatWindow from './components/ChatWindow';
+import ConversationItem from './components/ConversationItem';
+import ConversationList from './components/ConversationList';
+import MessageBubble from './components/MessageBubble';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [messageIn, setMessageIn] = useState("")
+
 }
 
 export default App;
---
+++ src/components/ChatInput.js
@@ -0,0 +1,21 @@
+export default function ChatInput({messageIn, setMessageIn, onSend}) {
+    return (
+        <div className="input-box">
+            <input
+                className="chat-input"
+                value={messageIn}
+                onChange={e => setMessageIn(e.target.value)}
+                onKeyDown={e => {
+                    if (e.key === "Enter" && !e.shiftKey) {
+                        e.preventDefault()
+                        onSend()
+                    }
+                }}
+                placeholder="Type a new message..."
+            />
+            <button className="send-button" onClick={onSend}>
+                <img src="send-button.svg"/>
+            </button>
+        </div>
+    )
+}
