f69a403dbfc601b2425ed2685439a8484859d87e
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-06-05 20:51:43 -0500

    added chat window component

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,3 +1,23 @@
-export default function ChatWindow() {
+import ChatInput from "./ChatInput";
+import MessageBubble from "./MessageBubble";
 
+export default function ChatWindow({selected, conversations, messageIn, setMessageIn, onSend}) {
+    return (
+        <div className="chat-window">
+            <div className="chat-scroll-window">
+                {selected.messages.map((message, index) => (
+                    <MessageBubble 
+                        role={message.role}
+                        content={message.content}
+                        key={index}
+                    />
+                ))}
+            </div>
+            <ChatInput 
+                messageIn={messageIn}
+                setMessageIn={setMessageIn}
+                onSend={onSend}
+            />
+        </div>   
+    )
 }
