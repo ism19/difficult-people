19339546fe507554e4f2a2089ca02907609eb933
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-06-10 22:01:51 -0500

    added copy button for copying responses

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -1,8 +1,22 @@
 import '../style/MessageBubble.css'
+import { useState } from 'react'
 
 export default function MessageBubble({role, content}) {
+    const [copied, setCopied] = useState(false)
+
+    function copyText() {
+        navigator.clipboard.writeText(content)
+        setCopied(true)
+        setTimeout(() => setCopied(false), 2000)
+    }
+
     return role === "assistant" ? (
-        <p className="system-message">{content}</p>
+        <div className="system-message-wrapper">
+            <p className="system-message">{content}</p>
+            <button className="copy-button" onClick={copyText}>
+                <img src={copied ? "check-mark.svg" : "copy-button.svg"}/>
+            </button>
+        </div>
     ) : (
         <p className="user-message">{content}</p>
     )
